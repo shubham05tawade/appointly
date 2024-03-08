ee14724f21e7b1293019a470a1980cad480e99b0
Shubham <shubham05tawade@gmail.com>
2024-03-08 14:31:58 +0000

    mongodb connection and router initialization

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express= require('express');
+const userRoutes = require('./routes/User')
+require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send("Welcome to appointly!!");
-})
+app.use('/user', userRoutes)
 
 app.listen(PORT, () => {
-    console.log('Connected to node server!!')
+    console.log('Connected to node server...')
 })
